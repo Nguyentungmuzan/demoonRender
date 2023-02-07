2eefefb0b4dc389a94a2ebb1a500d24c80c703da
Nguyễn Duy Lâm Tùng <102568569+Nguyentungmuzan@users.noreply.github.com>
2023-02-07 11:07:08 +0700

    Update
    ok

--- main2.js
+++ main2.js
@@ -1,18 +1,20 @@
 var express = require('express')
 var app = express()
 
-app.get('/',function(req,res){
-    res.writeHead(200, { 'Content-Type': 'text/html' }); 
+app.get('/', function(req, res){
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write('<html><body><p>This is home Page.</p></body></html>');
+    res.write('<p><a href="http://localhost:7000/student">Student</a></p>\n');
     res.end();
 })
 
-app.get('/student',(req,res)=>{
-    res.writeHead(200, { 'Content-Type': 'text/html' }); 
-    res.write('<html><body><p>This is the student page.</p></body></html>');
+app.get('/student', (req, res)=>{
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<html><body><p>This is student Page.</p></body></html>');
+    res.write('<p><a href="http://localhost:7000">Back to home page</a></p>\n');
     res.end();
 })
 
 const PORT = process.env.PORT || 8000
 app.listen(PORT)
-console.log('Server is up!')
+console.log('Sever is up!')
