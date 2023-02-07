ac62a78ccaf4138f77c0e8e165193e77f2c67710
Nguyễn Duy Lâm Tùng <102568569+Nguyentungmuzan@users.noreply.github.com>
2023-02-07 11:35:17 +0700

    ẻt
    ẻt

--- main2.js
+++ main2.js
@@ -1,20 +1,19 @@
-var express = require('express')
-var app = express()
-
-app.get('/', function(req, res){
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<html><body><p>This is home Page.</p></body></html>');
-    res.write('<p><a href="http://localhost:8000/student">Student</a></p>\n');
-    res.end();
+var express = require('express');
+var app = express();
+app.get('/', function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<html><body><p>This is home Page.</p></body></html>');
+        res.end('<p><a href="/student">Click here!</a></p>\n');
+        // res.end();
 })
 
-app.get('/student', (req, res)=>{
+app.get('/student', function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<html><body><p>This is student Page.</p></body></html>');
-    res.write('<p><a href="http://localhost:8000">Back to home page</a></p>\n');
-    res.end();
+        res.write('<html><body><p>This is student Page.</p></body></html>');
+        res.end('<p><a href="/">Click here!</a></p>\n');
+
 })
 
 const PORT = process.env.PORT || 8000
 app.listen(PORT)
-console.log('Sever is up!')
+console.log('Server is up')
